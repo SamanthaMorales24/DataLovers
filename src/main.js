1f9c8bc31d4fbf74607667be98a04eba95282c53
RominaVargas <rominita.malfoy@gmail.com>
2022-08-02 18:13:37 -0400

    sin libro actualización data con id

--- src/main.js
+++ src/main.js
@@ -1,23 +1,52 @@
 import { example } from './data.js';
 import data from './data/harrypotter/data.js';
-console.log(data.potions);
+//console.log(data.potions);
 
 const potions = data.potions;
+const descData = data.potions.description;
 
 
 for(let i= 0; i< potions.length; i++ ){
-    let potionsData = `<li>${potions[i].name}</li>`;
+    let potionsData = `<li><a href="#libro" id=" ${potions[i].id}">${potions[i].name}</a></li>`;
     
-
     document.getElementById("potions").innerHTML += potionsData;
-    console.log(potions[i].name)
+    
+    for(let i= 0; i< potions.length; i++ ){
+        let descData = `<p>${potions[i].description}</p>`;
+        document.getElementById("libro").innerHTML += descData;
+    }
+    
 }
+    
+    
+    console.log(potionsData);
+
+
+
+
+
+
+
+
+
+/*
+
+function init() {
+ 
+    let a = document.createElement("a");
+    a.setAttribute("href", "libro");
+    let aTexto = document.createTextNode("Google");
+    a.appendChild(aTexto);
+     
+    document.body.appendChild(a);
+ 
+}
+ 
+window.onload = init; */
 /*
 for(let i= 0; i< potions.length; i++ ){
     let potionsData = `<p>${potions[i].description}</p>`;
     
 
-    document.getElementById("potions").innerHTML += potionsData;
-    console.log(potions[i].name)
-}
+    
 */

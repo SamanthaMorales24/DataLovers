26d100715fc7adf67866df41a0499144344da337
SamanthaM <morales.samanthaa1@gmail.com>
2022-08-08 09:35:16 -0400

    cambios en html h

--- src/main.js
+++ src/main.js
@@ -6,47 +6,57 @@ const potions = data.potions;
 const descData = data.potions.description;
 
 
+
+
 for(let i= 0; i< potions.length; i++ ){
-    let potionsData = `<li><a href="#libro" id=" ${potions[i].id}">${potions[i].name}</a></li>`;
-    
-    document.getElementById("potions").innerHTML += potionsData;
-    
-    for(let i= 0; i< potions.length; i++ ){
-        let descData = `<p>${potions[i].description}</p>`;
-        document.getElementById("libro").innerHTML += descData;
-    }
+    let potionsData = `<li><button name"${potions[i].id}" id= "indiceX" value="${potions[i].name}" >${potions[i].name}</button></li>`; 
+    document.getElementById("potions").innerHTML += potionsData; 
     
 }
-    
-    
-    console.log(potionsData);
+
+// href="#libro"
+document.getElementById("indiceX").addEventListener(
+    "click",
+    function () {
+        let id = document.getElementById("indiceX").value;
+        console.log(id);
+    },
+    true
+  );
+
+
+// const traerDescripcion = (value) => {
+//     let id2 = value;
+//     console.log(id2);
+//     // let id = potions.find(e => e =)
+//     // let descripcion = `<p>${potions[].description}</p>`;
+//     // document.getElementById("libro").innerHTML += descData;
+// }
+
+
+
+// for(let i= 0; i< potions.length; i++ ){
+//     let descData = `<p>${potions[i].description}</p>`;
+//     document.getElementById("libro").innerHTML += descData;
+//     if (i < 150 ){
+//         break
+//     }     
+// }
 
 
 
 
-
-
-
-
-
+   // console.log(potionsData);
 /*
-
 function init() {
- 
     let a = document.createElement("a");
     a.setAttribute("href", "libro");
     let aTexto = document.createTextNode("Google");
     a.appendChild(aTexto);
-     
     document.body.appendChild(a);
- 
 }
- 
 window.onload = init; */
 /*
 for(let i= 0; i< potions.length; i++ ){
-    let potionsData = `<p>${potions[i].description}</p>`;
-    
-
-    
+    let potionsData = `<p>${potions[i].description}</p>`; 
 */

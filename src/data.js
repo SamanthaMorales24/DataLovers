d013d9e2e1f94f4e3e8a04e3fcfde1863d172f1f
RominaVargas <rominita.malfoy@gmail.com>
2022-08-18 15:12:14 -0400

    funcions sort para todas las propiedades

--- src/data.js
+++ src/data.js
@@ -22,7 +22,7 @@ const orderZA = name.sort((a, b) => b - a);
 return orderZA;
 };
 */
-
+/*
 const sortAZ=(Data,sortBy) => {
   let orderAZ = name.sort((a, b) => {
     if (a[sortBy] > b[sortBy]) {
@@ -48,33 +48,69 @@ const sortZA=(Data,sortBy) => {
   return orderZA;
 }
 export { sortZA };
+*/
 
-/*
-export function sortAZ(name){
-  let orderAZ = name.sort((a, b) => (a.name > b.name ? 1 : -1));
-  console.log(orderAZ)
-  return orderAZ;
+export function sortAZC(name) {
+  let orderAZC = name.sort((a, b) => (a.name > b.name ? 1 : -1));
+  console.log(orderAZC)
+  return orderAZC;
 }
 
-export function sortZA(name){
-  let orderZA = name.sort((a, b) => (a.name > b.name ? -1 : 1));
-  console.log(orderZA)
-  return orderZA;
-}*/
- /*
+export function sortZAC(name) {
+  let orderZAC = name.sort((a, b) => (a.name > b.name ? -1 : 1));
+  console.log(orderZAC)
+  return orderZAC;
+}
+
+export function sortAZP(name) {
+  let orderAZP = name.sort((a, b) => (a.name > b.name ? 1 : -1));
+  console.log(orderAZP)
+  return orderAZP;
+}
+
+export function sortZAP(name) {
+  let orderZAP = name.sort((a, b) => (a.name > b.name ? -1 : 1));
+  console.log(orderZAP)
+  return orderZAP;
+}
+export function sortAZH(name) {
+  let orderAZH = name.sort((a, b) => (a.name > b.name ? 1 : -1));
+  console.log(orderAZH)
+  return orderAZH;
+}
+
+export function sortZAH(name) {
+  let orderZAH = name.sort((a, b) => (a.name > b.name ? -1 : 1));
+  console.log(orderZAH)
+  return orderZAH;
+}
+export function sortAZF(name) {
+  let orderAZF = name.sort((a, b) => (a.name > b.name ? 1 : -1));
+  console.log(orderAZF)
+  return orderAZF;
+}
+
+export function sortZAF(name) {
+  let orderZAF = name.sort((a, b) => (a.name > b.name ? -1 : 1));
+  console.log(orderZAF)
+  return orderZAF;
+}
+
+
+/*
 export const ordenarAtoZ = (a, b) => {
-  const pocionesAZ = potions.function (a, b) {
-    return a.localeCompare(b);
+const pocionesAZ = potions.function (a, b) {
+ return a.localeCompare(b);
 
-  pociones.sort()
-  return a - b;
+pociones.sort()
+return a - b;
 
-  console.log(pociones)
+console.log(pociones)
 }*/
 
 // export const sortDataUp = (potions) => {
 //   let filterByAZ = potions.sort((a,b) => a.potions.localeCompare(b.potions));
-//   return filterByAZ;  
+//   return filterByAZ;
 // }
 
 

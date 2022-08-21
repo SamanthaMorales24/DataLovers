3937c2ecf337e5e3c632f73e30953f79f7148e19
SamanthaM <morales.samanthaa1@gmail.com>
2022-08-21 16:03:57 -0400

    filtro funcionando

--- src/main.js
+++ src/main.js
@@ -1,27 +1,21 @@
-//importar funciones y data
 import { filtroEspecie, sortAZ, sortZA } from "./data.js";
 import data from "./data/harrypotter/data.js";
 
-//generamos variables para las datas que se verán en la página al seleccionar
 const characters = [...data.characters];
 const potions = [...data.potions];
 const spells = [...data.spells];
 const funFacts = [...data.funFacts];
-const noHumanos = filtroEspecie(characters);
-//data vacía para que se trabaje con la data que se muestre en pantalla.
 let renderData = [];
 
-//variable que limpia la página de información con div vacio.
 const limpiar = `<div></div>`;
+const noHumanos = filtroEspecie(characters);
 
-//función que carga la información de pociones
 const renderItemPotions = (data) => {
-    let id = data.id - 1;
-    let nombre = potions[id].name;
-    let descripcion = potions[id].description;
+  let id = data.id - 1;
+  let nombre = potions[id].name;
+  let descripcion = potions[id].description;
 
-    //interpolación que crea los divs con información de cada data con las propiedades descritas.
-    let poster = `
+  let poster = `
     
     <div class="container" id="tarjetita">
         <div class="card">
@@ -33,26 +27,24 @@ const renderItemPotions = (data) => {
     </div> 
     `;
 
-    document.getElementById("libro").innerHTML += poster;
-    renderData[id] = data;
+  document.getElementById("libro").innerHTML += poster;
+  renderData[id] = data;
 };
 
-//función que carga la información de hechizos.
 const renderItemSpells = (data) => {
-    let id = data.id - 1;
-    let nombre = spells[id].name;
-    let otroNombre = spells[id].other_name;
-    if (otroNombre === null) {
-        otroNombre = "Información no disponible";
-    }
-    let pronunciacion = spells[id].pronunciation;
-    let tipoHechizo = spells[id].spell_type;
-    let descripcion = spells[id].description;
-    let mencion = spells[id].mention;
-    let etimologia = spells[id].etymology;
+  let id = data.id - 1;
+  let nombre = spells[id].name;
+  let otroNombre = spells[id].other_name;
+  if (otroNombre === null) {
+    otroNombre = "Información no disponible";
+  }
+  let pronunciacion = spells[id].pronunciation;
+  let tipoHechizo = spells[id].spell_type;
+  let descripcion = spells[id].description;
+  let mencion = spells[id].mention;
+  let etimologia = spells[id].etymology;
 
-    //interpolación con span para que no deforme la página con exceso de texto.
-    let poster = `
+  let poster = `
     <div class="container" id="tarjetita">
     <div class="card">
     <img id="fotoHechizo" src= "./images/hechizofoto.jpg" width="350" height="350">
@@ -69,18 +61,17 @@ const renderItemSpells = (data) => {
             </div>
         </div> 
         `;
-    //esto imprime las targetas en el contenedor de la página.
-    document.getElementById("libro").innerHTML += poster;
-    renderData[id] = data;
+
+  document.getElementById("libro").innerHTML += poster;
+  renderData[id] = data;
 };
 
-//función que carga la información de datos curiosos.
 const renderItemFunFacts = (data) => {
-    let id = data.id - 1;
-    let tipo = funFacts[id].type;
-    let contenido = funFacts[id].content;
+  let id = data.id - 1;
+  let tipo = funFacts[id].type;
+  let contenido = funFacts[id].content;
 
-    let poster = `
+  let poster = `
     <div class="container" id="tarjetita">
     <div class="card">
     <img id="fotoDato" src="./images/libroDato.jpg" width="350" height="350">
@@ -91,18 +82,18 @@ const renderItemFunFacts = (data) => {
     
         `;
 
-    document.getElementById("libro").innerHTML += poster;
-    renderData[id] = data;
+  document.getElementById("libro").innerHTML += poster;
+  renderData[id] = data;
 };
-//función que carga la información de criaturas mágicas.
-const renderItemSpecies = (data) => {
-    let id = data.id - 1;
-    let nombre = data.name;
-    let especies = data.species;
-    let ojos = data.eye_color;
-    let pelaje = data.hair_color;
 
-    let poster = `
+const renderItemSpecies = (data) => {
+  let id = data.id - 1;
+  let nombre = data.name;
+  let especies = data.species;
+  let ojos = data.eye_color;
+  let pelaje = data.hair_color;
+
+  let poster = `
     <div class="container" id="tarjetita">
     <div class="card">
     <img id="fotoCriatura" src="./images/bestia.jpg" width="350" height="350">
@@ -113,79 +104,85 @@ const renderItemSpecies = (data) => {
        
        `;
 
-    document.getElementById("libro").innerHTML += poster;
-    renderData[id] = data;
+  document.getElementById("libro").innerHTML += poster;
+  renderData[id] = data;
 };
-//acción para leer más en data hechizo.
+
 window.addEventListener("load", indice, leerMas, true);
 
-//función que carga la data cuando se hace click en la categoria.
 function indice() {
-    document.getElementById("pociones").addEventListener("click", function () {
-        data.pociones = document.getElementById("libro").value;
-        document.getElementById("libro").innerHTML = limpiar;
-        renderData = [];
-        potions.forEach((e) => renderItemPotions(e));
-        console.log(renderData);
-    });
+  document.getElementById("pociones").addEventListener("click", function () {
+    data.pociones = document.getElementById("libro").value;
+    document.getElementById("libro").innerHTML = limpiar;
+    renderData = [];
+    potions.forEach((e) => renderItemPotions(e));
+    console.log(renderData);
+  });
 
-    document.getElementById("hechizos").addEventListener("click", function () {
-        data.spells = document.getElementById("libro").value;
-        document.getElementById("libro").innerHTML = limpiar;
-        renderData = [];
-        spells.forEach((e) => renderItemSpells(e));
-        console.log(renderData);
+  document.getElementById("hechizos").addEventListener("click", function () {
+    data.spells = document.getElementById("libro").value;
+    document.getElementById("libro").innerHTML = limpiar;
+    renderData = [];
+    spells.forEach((e) => renderItemSpells(e));
+    console.log(renderData);
+  });
+  document
+    .getElementById("datosCuriosos")
+    .addEventListener("click", function () {
+      data.facts = document.getElementById("libro").value;
+      document.getElementById("libro").innerHTML = limpiar;
+      renderData = [];
+      funFacts.forEach((e) => renderItemFunFacts(e));
+      console.log(renderData);
     });
-    document
-        .getElementById("datosCuriosos")
-        .addEventListener("click", function () {
-            data.facts = document.getElementById("libro").value;
-            document.getElementById("libro").innerHTML = limpiar;
-            renderData = [];
-            funFacts.forEach((e) => renderItemFunFacts(e));
-            console.log(renderData);
-        });
-    document.getElementById("criaturas").addEventListener("click", function () {
-        data.characters = document.getElementById("libro").value;
-        document.getElementById("libro").innerHTML = limpiar;
-        renderData = [];
-        noHumanos.forEach((e) => renderItemSpecies(e));
-        console.log(renderData);
-    });
+  document.getElementById("criaturas").addEventListener("click", function () {
+    data.characters = document.getElementById("libro").value;
+    document.getElementById("libro").innerHTML = limpiar;
+    renderData = [];
+    noHumanos.forEach((e) => renderItemSpecies(e));
+    console.log(renderData);
+  });
 
-    /*
-    let select = document.getElementById('seleccion');
-    select.addEventListener('change', function () {
-        let selectedOption = this.options[select.selectedIndex];
-        console.log(selectedOption);
-    });
-        let selectAZ = document.getElementById("seleccion");
-        function select() {
-            console.log(selectAZ.selectedOptions[0].value);
-        };
-        document.getElementById("libro").innerHTML;
  
-        //   primero identificar que opcion del select está seleccionada
-        //   hacer un if para identificar que sort usar
-        console.log(selectAZ);
-        /*  if (selectAZ = "az") {
-              const orderAZ = sortAZ(renderData);
-              orderAZ.forEach((e) => renderItemPotions(e));
-          } else {
-              renderData.reverse();
-              //const orderZA = sortZA(renderData);
-              //orderZA.forEach((e) => renderItemPotions(e));
-          }
-        // console.log(renderData.reverse());
-    });*/
+  let select = document.querySelector('.seleccionOrden');
 
-};
+  select.addEventListener('change', (event) => {
+      let resultado = event.target.value;
+      console.log(resultado)
+      
+      switch (resultado) {
+        case 'az':
+          renderData = sortAZ(renderData);
+          document.getElementById("libro").innerHTML = limpiar;
+          data.az = document.getElementById("libro").value;
+          renderData.forEach((e) => renderItemPotions(e));
+          break;
+        case 'za':
+          renderData = sortZA(renderData);
+          document.getElementById("libro").innerHTML = limpiar;
+          data.za = document.getElementById("libro").value;
+          renderData.forEach((e) => renderItemPotions(e));
+          break;
+      }
+  });
+  //document.getElementById("botonOK").addEventListener("click", function () {
+  // primero identificar que opcion del select está seleccionada
+  // hacer un if para identificar que sort usar
+  // if (opcionSeleccionada = "az") {
+  //     let orderAZ = sortAZ(renderData);
+  //     orderAZ.forEach((e) => renderItemPotions(e));
+  // } else {
+  //     let orderZA = sortZA(renderData);
+  //     orderZA.forEach((e) => renderItemPotions(e));
+  //     }
+  //});
+}
 
 function leerMas() {
-    let hideText_btn = document.getElementById("hideText_btn");
-    let hideText = document.getElementById("hideText");
-    hideText_btn.addEventListener("click", toggleText);
-    function toggleText() {
-        hideText.classList.toggle("show");
-    }
+  let hideText_btn = document.getElementById("hideText_btn");
+  let hideText = document.getElementById("hideText");
+  hideText_btn.addEventListener("click", toggleText);
+  function toggleText() {
+    hideText.classList.toggle("show");
+  }
 }
